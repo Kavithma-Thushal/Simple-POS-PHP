5dbb483eb0edccc40a70b89631361004c8919641
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-10-10 10:16:17 +0530

    Refactored 2

--- BackEnd/util/schema.sql
+++
@@ -1,9 +0,0 @@
-USE demo;
-
-CREATE TABLE customers
-(
-    id      VARCHAR(10) PRIMARY KEY,
-    name    VARCHAR(100)   NOT NULL,
-    address VARCHAR(255)   NOT NULL,
-    salary  DECIMAL(10, 2) NOT NULL
-);
